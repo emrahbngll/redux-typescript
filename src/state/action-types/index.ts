05ab820f3e54f8018542f6fb875538a16cfaf239
emrahbnglll <gscimbommm842@gmail.com>
2022-12-27 23:00:27 +0000

    first commit

---
+++ src/state/action-types/index.ts
@@ -0,0 +1,5 @@
+export enum ActionType {
+  SEARCH_REPOSITORIES = 'search_repositorories',
+  SEARCH_REPOSITORIES_SUCCESS = 'search_repositorories_success',
+  SEARCH_REPOSITORIES_ERROR = 'search_repositorories_error',
+}
